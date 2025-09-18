2ff1906b954d6f5a087f6e476f05354098e7bd7b
saborno <saborno.ck@gmail.com>
2025-09-18 22:20:01 +0530

    Initial commit

---
+++ javascript/fees_script.js
@@ -0,0 +1,161 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+
+    if(inputVal==74){
+        document.getElementById("demo").innerHTML ="RUDRANIL GOSWAMI";
+        document.getElementById("demo1").innerHTML ="100000";
+    }
+    else if(inputVal==76)
+    {
+        document.getElementById("demo").innerHTML ="SABORNO CHAKRABORTY";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==3)
+    {
+        document.getElementById("demo").innerHTML ="ADREEJA NANDY";
+        document.getElementById("demo1").innerHTML ="FREE";   
+    }
+
+    else if(inputVal==68)
+    {
+        document.getElementById("demo").innerHTML ="RAMYADEEP GHOSHAL";
+        document.getElementById("demo1").innerHTML ="8320";   
+    }
+    else if(inputVal==4)
+    {
+        document.getElementById("demo").innerHTML ="SREERUPA CHOWDHURY";
+        document.getElementById("demo1").innerHTML ="7469";   
+    }
+    else if(inputVal==61)
+    {
+        document.getElementById("demo").innerHTML ="SWARUP KUMAR MITRA";
+        document.getElementById("demo1").innerHTML ="200000000";   
+    }
+    else if(inputVal==48)
+    {
+        document.getElementById("demo").innerHTML ="MANINDER SINGH";
+        document.getElementById("demo1").innerHTML ="6567";   
+    }
+    else if(inputVal==8)
+    {
+        document.getElementById("demo").innerHTML ="TONI KROOS";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+    else if(inputVal==10)
+    {
+        document.getElementById("demo").innerHTML ="LIONEL MESSI";
+        document.getElementById("demo1").innerHTML ="48938";   
+    }
+    else if(inputVal==7)
+    {
+        document.getElementById("demo").innerHTML ="CRISTIANO RONALDO";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==11)
+    {
+        document.getElementById("demo").innerHTML ="ANANAT AMBANI ";
+        document.getElementById("demo1").innerHTML ="4646347";   
+    }
+     else if(inputVal==12)
+    {
+        document.getElementById("demo").innerHTML ="ADITYA GUPTA";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==13)
+    {
+        document.getElementById("demo").innerHTML ="DIPPYAMAN BARUA";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==14)
+    {
+        document.getElementById("demo").innerHTML ="SWARAJ DHARA";
+        document.getElementById("demo1").innerHTML ="98393";   
+    }
+    else if(inputVal==15)
+    {
+        document.getElementById("demo").innerHTML ="SWARUP SAHA";
+        document.getElementById("demo1").innerHTML ="6";   
+    }
+    else if(inputVal==16)
+    {
+        document.getElementById("demo").innerHTML ="MAHATMA SALAH";
+        document.getElementById("demo1").innerHTML ="54678";   
+    }
+    else if(inputVal==17)
+    {
+        document.getElementById("demo").innerHTML ="R PARCHINA BHAI";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==18)
+    {
+        document.getElementById("demo").innerHTML ="SOMA BORAL";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==19)
+    {
+        document.getElementById("demo").innerHTML ="SARTHAK CHAKARBORTY";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==20)
+    {
+        document.getElementById("demo").innerHTML ="ROHITKUMAR";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+     else if(inputVal==21)
+    {
+        document.getElementById("demo").innerHTML ="SHARUKH KHAN";
+        document.getElementById("demo1").innerHTML ="1000"; 
+    }
+    else if(inputVal==22)
+    {
+        document.getElementById("demo").innerHTML ="HRITHIK ROSHAN";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==23)
+    {
+        document.getElementById("demo").innerHTML ="MODI JI";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==24)
+    {
+        document.getElementById("demo").innerHTML ="MAMATA DIDI";
+        document.getElementById("demo1").innerHTML ="-574577"; 
+    }
+    else if(inputVal==25)
+    {
+        document.getElementById("demo").innerHTML ="RAHUL GANDHI";
+        document.getElementById("demo1").innerHTML ="750000"; 
+    }
+    else if(inputVal==26)
+    {
+        document.getElementById("demo").innerHTML ="SOHAM GHOSH";
+        document.getElementById("demo1").innerHTML ="1000000"; 
+    }
+     else if(inputVal==27)
+    {
+        document.getElementById("demo").innerHTML ="RAMBISNU";
+        document.getElementById("demo1").innerHTML ="99999"; 
+    }
+    else if(inputVal==28)
+    {
+        document.getElementById("demo").innerHTML ="ABDUL HALIM";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+
+    else if(inputVal==29)
+    {
+        document.getElementById("demo").innerHTML ="SAYANATANI SIMLAI";
+        document.getElementById("demo1").innerHTML ="85787"; 
+    }
+     else if(inputVal==30)
+    {
+        document.getElementById("demo").innerHTML ="GNIT ALL";
+        document.getElementById("demo1").innerHTML ="98989898"; 
+    }
+    
+}
+
